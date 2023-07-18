5b77048035d71ead3c0bef98c220f0612497e9f8
animonte <montene.ani@gmail.com>
2023-07-18 16:28:27 +0200

    carga del csv

--- house_price.py
+++ house_price.py
@@ -4,9 +4,17 @@ import pandas as pd
 import numpy as np
 from datasets import load_dataset
 
+# Carga del CSV desde huggingface
+
+dataset = load_dataset("animonte/train_house_price")
+
 # Lectura del CSV desde un data set
-dataset = load_dataset("animonte/bank-state-data")
+
 df = pd.DataFrame(dataset["train"])
+
+
+
+# Interfaz gráfica del demo
 
 def greet(name):
     return "Hello " + name + "!!"
