492d5370e110454ee4f8b7b47b990a21683504b0
animonte <montene.ani@gmail.com>
2023-07-18 16:39:29 +0200

    Creación y entrenamiento del modelo XGBReggressor

--- house_price.py
+++ house_price.py
@@ -3,6 +3,10 @@ from gradio import components
 import pandas as pd 
 import numpy as np
 from datasets import load_dataset
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from xgboost import XGBRegressor
+
 
 # Carga del CSV desde huggingface
 
@@ -17,6 +21,24 @@ df = pd.DataFrame(dataset["train"])
 Select = ['GrLivArea', 'TotalBsmtSF', 'MoSold', 'YearBuilt', 'YearRemodAdd', 'LotFrontage', 'YrSold', 'Id', 'BsmtFinSF1','OverallQual']
 X = df.loc[:, Select ]  # Variables predictoras
 y = df['SalePrice']     # Variable objetivo
+
+# División del dataframe para evitar el sobreajuste
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+#Creamos una instancia con XGBClassifier
+model_XGB = XGBRegressor(n_estimators=30, max_depth=2, learning_rate=.2, random_state=42)
+
+#Entrenamos el modelo con los datos de entrenamiento
+model_XGB.fit(X_train,y_train)
+
+#Creamos el conjunto de entrenamiento
+prediction_XGB = model_XGB.predict(X_test)
+
+#Calculamos la puntuación con el conjunto de entrenamiento
+scoreR2_XGB = r2_score(y_test, prediction_XGB)
+
+print("Puntuación:", scoreR2_XGB)
 
 # Interfaz gráfica del demo
 
