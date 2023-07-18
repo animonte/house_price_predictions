4ba3f047cdbe8e91cf7b581b26f1c2f6cc2277d0
animonte <montene.ani@gmail.com>
2023-07-18 20:02:10 +0200

    Creación de la interfaz gráfica

--- house_price.py
+++ house_price.py
@@ -6,6 +6,8 @@ from datasets import load_dataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
+import datetime
+
 
 
 # Carga del CSV desde huggingface
@@ -18,8 +20,8 @@ df = pd.DataFrame(dataset["train"])
 
 # Selección de variables para el modelo
 
-Select = ['GrLivArea', 'TotalBsmtSF', 'MoSold', 'YearBuilt', 'YearRemodAdd', 'LotFrontage', 'YrSold', 'Id', 'BsmtFinSF1','OverallQual']
-X = df.loc[:, Select ]  # Variables predictoras
+columns = ['GrLivArea', 'TotalBsmtSF', 'MoSold', 'YearBuilt', 'YearRemodAdd', 'LotFrontage', 'YrSold', 'BsmtFinSF1','OverallQual']
+X = df.loc[:, columns ]  # Variables predictoras
 y = df['SalePrice']     # Variable objetivo
 
 # División del dataframe para evitar el sobreajuste
@@ -33,17 +35,33 @@ model_XGB = XGBRegressor(n_estimators=30, max_depth=2, learning_rate=.2, random_
 model_XGB.fit(X_train,y_train)
 
 #Creamos el conjunto de entrenamiento
-prediction_XGB = model_XGB.predict(X_test)
-
-#Calculamos la puntuación con el conjunto de entrenamiento
-scoreR2_XGB = r2_score(y_test, prediction_XGB)
-
-print("Puntuación:", scoreR2_XGB)
+# prediction_XGB = model_XGB.predict(X_test)
 
 # Interfaz gráfica del demo
 
-def greet(name):
-    return "Hello " + name + "!!"
+def ui_predict(Superficie_casa, Superficie_sotano, Anio_construccion, Anio_remodelacion, Frente_terreno, Superficie_construida_terminada, calidad_construccion):
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+    # Obtener la fecha y hora actual
+    fecha_actual = datetime.datetime.now()
+
+    # Obtener el mes y el año de la fecha actual
+    Mes_venta = fecha_actual.month
+    Anio_venta = fecha_actual.year
+
+    inputs = [[Superficie_casa, Superficie_sotano, Mes_venta, Anio_construccion, Anio_remodelacion, Frente_terreno, Anio_venta, Superficie_construida_terminada, calidad_construccion]]
+    inputs = pd.DataFrame(inputs, columns= columns)
+    # Predicción
+    predicciones = model_XGB.predict(inputs)
+
+    return predicciones[0]
+
+output = components.Textbox(label='Precio obtenido con el XG BOOST Regressor')
+
+demo = gr.Interface(
+    fn=ui_predict, 
+    inputs=['number', "number","number","number", "number","number", gr.Slider(1, 10, step=1)], 
+    outputs=output, 
+    allow_flagging="never"
+    )
+
+demo.launch()
